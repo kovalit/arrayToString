2af00507a77ad7abe4a8341809c047e257a5ed5c
kovalit <kovalit@mail.ru>
2016-08-11 10:47:08 +1000

    MODIFIED: main alogoritm

--- app.js
+++ app.js
@@ -5,14 +5,20 @@
      */
 
 
-    var array = [1,2,3,4,5,6,7,8];
+    var array = [1,3,4,5,6,7,8,10,11,12] ;
        
-    
+    var isArray = function (arr) {
+            var toString = {}.toString;
+            return toString.call(arr) == '[object Array]';
+    };
 
 
     var toString = function(arr) {
 
-   
+        if (!isArray(arr)){
+                return false;
+        }
+        
         var lenght = arr.length;
 
         var range = [];
@@ -25,16 +31,34 @@
                     firstInt = arr[i];
                     isFirst = false;
             }
+            
+            
+            var isLast = (i === (lenght - 1));
+            if (!isLast) {
+                isLast = (arr[i+1] - arr[i]) > 1;
+            }
 
-            var isLast = (arr[i+1]-arr[i]) > 1 || i === (lenght - 1);
             if (isLast) {
                     isFirst = true;
-                    range.push(firstInt + '-' + arr[i]);
-                }
+                    
+                    if (firstInt === arr[i]) {
+                            range.push(firstInt);
+                    }
+                    else if (arr[i] - firstInt === 1){
+                            range.push(firstInt);
+                            range.push(arr[i]);
+                    }
+                    else {
+                            range.push(firstInt + '-' + arr[i]);
+                    }
+
+            }
 
         }
 
-         console.log(range);  
+        var str = range.join(',');  
+        
+        console.log(str);
 
     }
 
